0282f779a16862c904434554f522ae82b7e8d647
Jonatan <j.lorentzson@outlook.com>
2025-09-16 16:04:14 +0200

    add basic setup

---
+++ src/components/sidebar.jsx
@@ -0,0 +1,12 @@
+export default function Sidebar() {
+    return (
+        <>
+            <div id='sidebar'> 
+                <header>
+                    <h1>Start design your dream kitchen!</h1>
+                </header>
+            <div id='sidebar-options'></div>
+            </div>
+        </>
+    )
+}
