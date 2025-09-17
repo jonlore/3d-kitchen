e154acbb4dc40c84785cb6f870bafd7d29389342
filiplyrheden <filip.lyrheden@gmail.com>
2025-09-17 13:22:49 +0200

    update color selection in Sidebar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
-import Sidebar from './components/sidebar'
-import Model from './components/model'
-import './App.css'
+import { useState } from "react";
+import Sidebar from "./components/Sidebar";
+import Model from "./components/Model";
+import "./App.css";
 
-function App() {
+export default function App() {
+  const [colorHex, setColorHex] = useState("#6BAA75"); // startfärg
 
   return (
-    <>
-      <main>
-        <Model />
-        <Sidebar />
-      </main>
-    </>
-  )
+    <main
+      style={{
+        display: "grid",
+        gridTemplateColumns: "1fr 320px", // modell till vänster, sidebar till höger
+        height: "100vh",
+      }}
+    >
+      <Model colorHex={colorHex} />
+      <Sidebar colorHex={colorHex} setColorHex={setColorHex} />
+    </main>
+  );
 }
-
-export default App
